6cf16713780e6ac0d7b33d7f356e93784c59ed24
Mark Burggraf <markb@mantisbible.com>
2025-09-20 18:54:51 -0500

    Add Node.js runtime Dockerfile

---
+++ dockerfiles/node-runtime/server.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Hello from Node.js!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
